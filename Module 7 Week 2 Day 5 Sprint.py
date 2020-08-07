b4e18960bfd97c8fad2173da86cf4714ffa69f03
CPTWEX012 <62343020+CPTWEX012@users.noreply.github.com>
2020-08-07 16:48:13 +0200

    Update Module 7 Week 2 Day 5 Sprint.py

--- Module 7 Week 2 Day 5 Sprint.py
+++ Module 7 Week 2 Day 5 Sprint.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 #Importin tweepy to access twitter
 import tweepy
 #importing json to write to ajson file
 import json
+#importing pandas to create graph
+import pandas as pd
 #importing matplotlib to plot graph
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 #impotrting tweepy to access twitter
 from tweepy import OAuthHandler
 #storing keys in public variables
@@ -22,26 +18,19 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 #getting user input on what to search for
-tweet_amount_input= input("Insert number of tweets you want displayed: ")
-hashtag = input("What hashtag do you want searched?: ")
-tweet_amount=int(tweet_amount_input)
+hashtag = "BLM"
+tweetamount=100
 #search input through api
-tweets = tweepy.Cursor(api.search,q=hashtag,lang="en",until="").items(tweet_amount)
-all_user_data = [[tweet.user.screen_name, tweet.user.location, tweet.user.created_at] for tweet in tweets]
+tweets = tweepy.Cursor(api.search,q=hashtag,lang="en",until="").items(tweetamount)
+alluserdata = [[tweet.user.screen_name, tweet.user.location, tweet.user.created_at] for tweet in tweets]
 #exporting results to a json folder
 with open('twitter.json', 'w') as f:
-    for tweet in tweepy.Cursor(api.search,q=hashtag,lang="en",until="").items(tweet_amount):
+    for tweet in tweepy.Cursor(api.search,q=hashtag,lang="en",until="").items(tweetamount):
         f.write(json.dumps(tweet._json)+"\n")
         print("Written to Json file")
+tweet_graph = pd.DataFrame(data = alluserdata,columns=['Tweeters', 'Location opf user','Time of tweet'])
 #displaying results on graph
-tweet_table.plot()
-plt.xlabel('Amount of tweets about topic'.format(tweet_amount))
-plt.ylabel('Date topic was tweeted about'.format(hashtag))
-title('Graph of tweets over time')
-
-
-# In[ ]:
-
-
-
-
+tweet_graph.plot()
+plt.xlabel('Amount of tweets about topic')
+plt.ylabel('Date topic was tweeted about')
+plt.title('Graph of tweets over time')
